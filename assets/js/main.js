053a3c6948d218282f7de69dd0c0c20b064ec7aa
Daniil Sydorenko <daniildeveloper@gmail.com>
2016-05-07 22:21:59 +0200

    Organize javascript code, fixes: mobileResponsive menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,140 +1,260 @@
 'use strict';
 
-$(function(){
-    var timelineBlocks = $('.cd-timeline-block'),
-        offset = 0.8;
+/**
+ * ==============================================================
+ * On page load
+ * ==============================================================
+ */
+$(document).ready(function () {
+    App.init();
 
-    //hide timeline blocks which are outside the viewport
-    hideBlocks(timelineBlocks, offset);
+    App.showResponsiveMobileMenu();
 
-    //on scolling, show/animate timeline blocks when enter the viewport
-    $(window).on('scroll', function(){
-        (!window.requestAnimationFrame)
-            ? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 100)
-            : window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset); });
+    App.body.on("click", function () {
+        App.fadeInMenuElements();
     });
 
-    function hideBlocks(blocks, offset) {
-        blocks.each(function(){
-            ( $(this).offset().top > $(window).scrollTop()+$(window).height()*offset ) && $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
-        });
-    }
+    /**
+     * Video background
+     */
+    App.scaleVideoContainer();
 
-    function showBlocks(blocks, offset) {
-        blocks.each(function(){
-            ( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
-        });
-    }
+    App.initBannerVideoSize('.video-container .poster img');
+    App.initBannerVideoSize('.video-container .filter');
+    App.initBannerVideoSize('.video-container video');
 
-    //
-    //var h = document.querySelector("#home").offsetHeight,
-    //    n = document.querySelector("nav"),
-    //    x = h - n.offsetHeight;
-    //if (s >= x) {
-    //    if (!$(n).hasClass("show")) {
-    //        $(n).addClass("show");
-    //    }
-    //} else {
-    //    if ($(n).hasClass("show")) {
-    //        $(n).removeClass("show");
-    //    }
-    //}
-
-
-    var shrinkHeader = 700;
-    $(window).scroll(function() {
-        var h = window.innerHeight;
-        var scroll = getCurrentScroll();
-
-        if (scroll > h) {
-            $("header").addClass("fixed");
-        } else {
-            $("header").removeClass("fixed");
-        }
-
-        if ( scroll >= shrinkHeader ) {
-            //$('.header').removeClass('fadeOut');
-            $('.header').addClass('animated fadeInDown sd');
-        }
-        else {
-            //$(".header").slideUp(1000, function() {
-            //    $('.header').removeClass('animated fadeInDown sd');
-            //});
-            $('.header').removeClass('fadeInDown');
-
-            //$('.header').addClass('animated fadeOut');
-
-        }
-    });
-    function getCurrentScroll() {
-        return window.pageYOffset || document.documentElement.scrollTop;
-    }
-
-    $(".menu-btn-wrapper").on("click", function () {
-        $(".mobileMenuOverlay").addClass("flex");
-
-        setTimeout(function(){
-            $(".mobileMenuOverlay").addClass("display");
-
-            setTimeout(function(){
-            }, 420);
-        }, 20);
+    $(window).on('resize', function () {
+        App.scaleVideoContainer();
+        App.scaleBannerVideoSize('.video-container .poster img');
+        App.scaleBannerVideoSize('.video-container .filter');
+        App.scaleBannerVideoSize('.video-container video');
     });
 
-    function hideMobileMenuItems () {
-        var items = $(".mobileMenu li");
-        $(items[0]).removeClass("show");
-        setTimeout(function(){
-            $(items[1]).removeClass("show");
-            setTimeout(function(){
-                $(items[2]).removeClass("show");
-            }, 75);
-        }, 75);
-    }
-
-    function showMobileMenuItems () {
-        var items = $(".mobileMenu li");
-        setTimeout(function(){
-            $(items[0]).addClass("show");
-            setTimeout(function(){
-                $(items[1]).addClass("show");
-                setTimeout(function(){
-                    $(items[2]).addClass("show");
-                }, 75);
-            }, 75);
-        }, 100);
-    }
-
-    function toggleMobileMenu () {
-        if (!animationRunning) {
-            toggleMenuButton();
-            if (mobileMenuVisible) {
-                // Hide menu
-                mobileMenuVisible = false;
-                animationRunning = true;
-                hideMobileMenuItems();
-                setTimeout(function(){
-                    $(".mobileMenuOverlay").removeClass("show");
-                    setTimeout(function(){
-                        $(".mobileMenuOverlay").removeClass("display");
-                        animationRunning = false;
-                    }, 420);
-                }, 100);
-            } else {
-                // Show menu
-                mobileMenuVisible = true;
-                animationRunning = true;
-                $(".mobileMenuOverlay").addClass("display");
-                setTimeout(function(){
-                    showMobileMenuItems();
-                    $(".mobileMenuOverlay").addClass("show");
-                    setTimeout(function(){
-                        animationRunning = false;
-                    }, 420);
-                }, 20);
-
-            }
-        }
-    }
+    /**
+     * Timeline
+     */
+    App.hideBlocks(App.timelineBlocks, App.timelineOffset);
 });
 
+
+/**
+ * ==============================================================
+ * On window scroll
+ * ==============================================================
+ */
+$(window).on("scroll", function () {
+
+    /**
+     * Header animation
+     */
+    if (App.getCurrentScroll() >= App.shrinkHeader) {
+        $('.header').addClass('fixed fadeInDown');
+    } else {
+        $('.header').removeClass('fixed fadeInDown');
+    }
+
+    /**
+     * Timeline
+     */
+    (!window.requestAnimationFrame)
+        ? setTimeout(function () {
+        App.showBlocks(App.timelineBlocks, App.timelineOffset);
+    }, 100)
+        : window.requestAnimationFrame(function () {
+        App.showBlocks(App.timelineBlocks, App.timelineOffset);
+    });
+
+});
+
+/**
+ * Base application object
+ * Consists all needed functionality:
+ *  - Video bg
+ *  - Header
+ *  - Timeline
+ *  - Projects
+ **/
+
+var App = {
+
+    /* DOM selectors */
+    html: $('html'),
+    body: $('body'),
+    header: $('.header'),
+    footer: $('.footer'),
+    cookies: $('.cookies'),
+
+    /* Viewport/Window params */
+    ih: window.innerHeight,
+    oh: window.outerHeight,
+    iw: window.innerWidth,
+    ow: window.outerWidth,
+
+    /* Const values */
+    shrinkHeader: 700,
+
+    /* Timeline */
+    timelineBlocks: $('.cd-timeline-block'),
+    timelineOffset: 0.8,
+
+    /**
+     * Custom functions
+     */
+    getWindowHeight: function () {
+        return App.oh;
+    },
+    getViewportHeight: function () {
+        return App.ih;
+    },
+    getCurrentScroll: function () {
+        return window.pageYOffset || document.documentElement.scrollTop;
+    },
+
+    /**
+     * Responsive mobile menu
+     */
+    showResponsiveMobileMenu: function () {
+        $(".menu-btn-wrapper").on("click", function () {
+            $(".responsiveMobileMenu").addClass("flex");
+
+            setTimeout(function () {
+                $(".responsiveMobileMenu").addClass("display");
+
+                setTimeout(function () {
+                }, 420);
+            }, 20);
+        })
+    },
+
+    fadeOutMenuElements: function () {
+
+        var el = $(".mobileMenu li");
+
+        $(el[0]).removeClass("show");
+        setTimeout(function () {
+            $(el[1]).removeClass("show");
+            setTimeout(function () {
+                $(el[2]).removeClass("show");
+            }, 75);
+        }, 75);
+    },
+
+    fadeInMenuElements: function () {
+
+        var el = $(".mobileMenu li");
+
+        setTimeout(function () {
+            $(el[0]).addClass("fade");
+            setTimeout(function () {
+                $(el[1]).addClass("fade");
+                setTimeout(function () {
+                    $(el[2]).addClass("fade");
+                }, 100);
+            }, 150);
+        }, 200);
+    },
+
+    //toggleMobileMenu: function () {
+    //
+    //    if (!animationRunning) {
+    //        toggleMenuButton();
+    //        if (mobileMenuVisible) {
+    //            // Hide menu
+    //            mobileMenuVisible = false;
+    //            animationRunning = true;
+    //            hideMobileMenuItems();
+    //            setTimeout(function () {
+    //                $(".responsiveMobileMenu").removeClass("show");
+    //                setTimeout(function () {
+    //                    $(".responsiveMobileMenu").removeClass("display");
+    //                    animationRunning = false;
+    //                }, 420);
+    //            }, 100);
+    //        } else {
+    //            // Show menu
+    //            mobileMenuVisible = true;
+    //            animationRunning = true;
+    //            $(".responsiveMobileMenu").addClass("display");
+    //            setTimeout(function () {
+    //                showMobileMenuItems();
+    //                $(".responsiveMobileMenu").addClass("show");
+    //                setTimeout(function () {
+    //                    animationRunning = false;
+    //                }, 420);
+    //            }, 20);
+    //
+    //        }
+    //    }
+    //},
+
+    /**
+     * Video background
+     */
+    scaleVideoContainer: function () {
+
+        var height = App.ih + 5;
+        var unitHeight = parseInt(height) + 'px';
+        $('.homepage-hero-module').css('height', unitHeight);
+    },
+
+    initBannerVideoSize: function (element) {
+
+        $(element).each(function () {
+            $(this).data('height', $(this).height());
+            $(this).data('width', $(this).width());
+        });
+
+        this.scaleBannerVideoSize(element);
+    },
+
+    scaleBannerVideoSize: function (element) {
+
+        var windowWidth = App.iw,
+            windowHeight = App.ih + 5,
+            videoWidth,
+            videoHeight;
+
+        $(element).each(function () {
+            var videoAspectRatio = $(this).data('height') / $(this).data('width');
+
+            $(this).width(windowWidth);
+
+            if (windowWidth < 1000) {
+                videoHeight = windowHeight;
+                videoWidth = videoHeight / videoAspectRatio;
+                $(this).css({'margin-top': 0, 'margin-left': -(videoWidth - windowWidth) / 2 + 'px'});
+
+                $(this).width(videoWidth).height(videoHeight);
+            }
+
+            $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
+
+        });
+    },
+
+    /**
+     * Timeline
+     */
+    hideBlocks: function (blocks, offset) {
+        blocks.each(function () {
+            ( $(this).offset().top > $(window).scrollTop() + App.ih * offset ) &&
+            $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
+        });
+    },
+
+    showBlocks: function (blocks, offset) {
+        blocks.each(function () {
+            ( $(this).offset().top <= $(window).scrollTop() + App.ih * offset &&
+            $(this).find('.cd-timeline-img').hasClass('is-hidden') ) &&
+            $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+        });
+    },
+
+    /**
+     * Initialization
+     */
+    init: function () {
+        // Add logic ...
+    }
+
+};
