4f07a68830b281263af56333622fae704a0e7a6f
Daniil Sydorenko <daniildeveloper@gmail.com>
2016-11-06 13:06:20 +0100

    Clean commit

--- app/components/worksPage/worksPage.ctrl.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    'use strict';
-
-    angular.module('portfolioApp').controller('WorksController', function ($scope, $routeParams) {
-        $scope.message = "WORKS PAGE";
-        $scope.workId = $routeParams.id;
-    });
-})();
